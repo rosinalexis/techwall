374f8751e05017997a53e757b9c43231fb589577
rosinalexis <rosinalexis@yahoo.fr>
2022-10-26 23:24:54 +0200

    ajout de la paritie get todo by id

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,41 +1,50 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {Body, Controller, Delete, Get, NotFoundException, Param, Post, Put} from '@nestjs/common';
 import {Todo} from "./entities/todo.entity";
 
 
 @Controller('todo')
 export class TodoController {
 
-  todos: Todo[];
+    todos: Todo[];
 
-  constructor() {
-    this.todos = []
-  }
+    constructor() {
+        this.todos = []
+    }
 
+    @Get()
+    getAllTodos() {
+        return this.todos;
+    }
 
-  @Get()
-  getAllTodos() {
-    return this.todos;
-  }
+    @Get('/:id')
+    getTodoById(@Param('id') id: string) {
+        
+        const todo = this.todos.find(t => t.id === +id);
+        if (todo) {
+            return todo;
+        }
+        throw new NotFoundException(`le todo avec l'id: ${id} n'existe pas.`);
+    }
 
-  @Post()
-  addTodo(@Body() newTodo:Todo) {
-   if(this.todos.length){
-     newTodo.id =this.todos[this.todos.length-1].id+1;
-   }else{
-     newTodo.id =1;
-   }
-   this.todos.push(newTodo);
-  }
+    @Post()
+    addTodo(@Body() newTodo: Todo) {
+        if (this.todos.length) {
+            newTodo.id = this.todos[this.todos.length - 1].id + 1;
+        } else {
+            newTodo.id = 1;
+        }
+        this.todos.push(newTodo);
+    }
 
-  @Delete()
-  deleteTodo() {
-    console.log('Supprimer un todo.');
-    return 'Supprimer un todo.';
-  }
+    @Delete()
+    deleteTodo() {
+        console.log('Supprimer un todo.');
+        return 'Supprimer un todo.';
+    }
 
-  @Put()
-  modifyTodo() {
-    console.log('Modifier un todo.');
-    return 'Modifier todo.';
-  }
+    @Put()
+    modifyTodo() {
+        console.log('Modifier un todo.');
+        return 'Modifier todo.';
+    }
 }
